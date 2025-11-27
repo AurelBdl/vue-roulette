847c2fc03a3853c50fb085f93d68bdb39918fe42
Aurelien <blondel.aurelien.pro@gmail.com>
2025-11-27 11:12:24 +0100

    Fix: demo deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,21 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  preview: {
-		port: 3000,
-		host: true,    // This enables listening on all network interfaces
-	},
-	server: {        // Also add this for development server
-		host: true,    // This enables listening on all network interfaces
-		port: 3000
-	},
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'CasinoRoulette',
+      fileName: format => (format === 'es' ? 'casino-roulette.js' : 'casino-roulette.umd.cjs'),
+      formats: ['es', 'umd']
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        exports: 'named',
+        globals: {
+          vue: 'Vue'
+        }
+      }
+    }
+  }
 })
