5a7c43dc6a8c3125232246ff63fab004066ca6b3
Aurelien <blondel.aurelien.pro@gmail.com>
2025-11-27 10:56:36 +0100

    Fix: demo deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  preview: {
+		port: 3000,
+		host: true,    // This enables listening on all network interfaces
+	},
+	server: {        // Also add this for development server
+		host: true,    // This enables listening on all network interfaces
+		port: 3000
+	},
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
