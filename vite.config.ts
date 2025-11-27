6f09163e266a2cd9d56917b33f9140982ea40035
Aurelien <blondel.aurelien.pro@gmail.com>
2025-11-27 10:43:38 +0100

    Fix: demo deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,31 +2,44 @@ import { resolve } from 'node:path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default defineConfig({
-  plugins: [vue()],
-  server: {
-		port: 3000,
-		host: true,
-	},
-	preview: {
-		port: 3000,
-		host: true,
-	},
-  // build: {
-  //   lib: {
-  //     entry: resolve(__dirname, 'src/index.ts'),
-  //     name: 'CasinoRoulette',
-  //     fileName: format => (format === 'es' ? 'casino-roulette.js' : 'casino-roulette.umd.cjs'),
-  //     formats: ['es', 'umd']
-  //   },
-  //   rollupOptions: {
-  //     external: ['vue'],
-  //     output: {
-  //       exports: 'named',
-  //       globals: {
-  //         vue: 'Vue'
-  //       }
-  //     }
-  //   }
-  // }
+export default defineConfig(({ command }) => {
+  const isBuildLib = command === 'build' && process.env.BUILD_MODE === 'lib'
+
+  return {
+    plugins: [vue()],
+
+    // Si on build la lib → config lib
+    ...(isBuildLib
+      ? {
+          build: {
+            lib: {
+              entry: resolve(__dirname, 'src/index.ts'),
+              name: 'CasinoRoulette',
+              fileName: format => (format === 'es' ? 'casino-roulette.js' : 'casino-roulette.umd.cjs'),
+              formats: ['es', 'umd']
+            },
+            rollupOptions: {
+              external: ['vue'],
+              output: {
+                exports: 'named',
+                globals: {
+                  vue: 'Vue'
+                }
+              }
+            }
+          }
+        }
+      : {
+          // Sinon → build classique pour la démo
+          build: {
+            outDir: 'dist-demo'
+          },
+          server: {
+            host: '0.0.0.0'
+          },
+          preview: {
+            host: '0.0.0.0'
+          }
+        })
+  }
 })
